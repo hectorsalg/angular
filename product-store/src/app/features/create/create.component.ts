c89c070462123f3cf4b659d4fd8a0bfdbff1d491
hectorsalg <hectorsalg@ufpi.edu.br>
2025-01-14 16:52:41 -0300

    feat: use de component back-to-list

--- product-store/src/app/features/create/create.component.ts
+++ product-store/src/app/features/create/create.component.ts
@@ -1,15 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductsService } from '../../shared/services/products.service';
 import { Router } from '@angular/router';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Product } from '../../shared/interfaces/product.interface';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
