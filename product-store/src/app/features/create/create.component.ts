cdcc0ea1799fb084922e6e918d85c5b0108a37d9
hectorsalg <hectorsalg@ufpi.edu.br>
2025-01-14 08:55:01 -0300

    feat: add funcionality to save product

--- product-store/src/app/features/create/create.component.ts
+++ product-store/src/app/features/create/create.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-create',
@@ -12,13 +13,20 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  productService = inject(ProductsService);
+
   form = new FormGroup({
-    title: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]}),
+    title: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
     price: new FormControl<number>(0, { nonNullable: true, validators: [Validators.required] }),
   });
 
   onSubmit() {
-    this.form.controls.title.value;
-    this.form.controls.price.value;
+    this.productService.post({
+      title: this.form.controls.title.value,
+      price: this.form.controls.price.value
+    }).subscribe(() => {
+      alert('Product created');
+    });
   }
 }
