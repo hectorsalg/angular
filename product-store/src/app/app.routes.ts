f2799e0a538d9838324ced70a8d37c3ad8e00554
hectorsalg <hectorsalg@ufpi.edu.br>
2025-01-14 15:40:34 -0300

    refactor: add resolvers

--- product-store/src/app/app.routes.ts
+++ product-store/src/app/app.routes.ts
@@ -1,34 +1,29 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
-import { ProductsService } from './shared/services/products.service';
-import { inject } from '@angular/core';
+import { getProducts } from './shared/resolvers/get-products.resolver';
+import { getProduct } from './shared/resolvers/get-product.resolver';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
     path: '',
     resolve: {
-        products: () => {
-            const productsService = inject(ProductsService);
-
-            return productsService.getAll();
-        },
+      products: getProducts,
     },
     component: ListComponent,
-},
-{
+  },
+  {
     path: 'create-product',
     loadComponent: () =>
-        import('./features/create/create.component').then(m => m.CreateComponent),
-},
-{
+      import('./features/create/create.component').then(
+        (m) => m.CreateComponent
+      ),
+  },
+  {
     path: 'edit-product/:id',
     resolve: {
-        product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-            const productsService = inject(ProductsService);
-
-            return productsService.get(route.paramMap.get('id') as string);
-        },
+      product: getProduct,
     },
     loadComponent: () =>
-        import('./features/edit/edit.component').then(m => m.EditComponent),
-}
+      import('./features/edit/edit.component').then((m) => m.EditComponent),
+  },
 ];
